32516497760112d163ae3ae32702d649322d434a
sanabriavalot <126500371+sanabriavalot@users.noreply.github.com>
2023-03-02 04:40:26 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+/* color palette:
+#082B59 bright blue
+#131D26 navy blue
+#59331D coffee
+#D98555 peach
+#F2F2F2 white
+
+Fonts:
+Headlines - Poppins Bold
+Text - Lato
+
+Navbar links should be correct to their respective pages.
+
+Social media links may go to the home page of each service.
+
+*/
